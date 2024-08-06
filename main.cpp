36825f3b9de30617c79c9a87a199f62c421c1eb5
Lian Tume <lian.tume@pucp.edu.pe>
2024-08-06 00:16:09 -0500

    moticommit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 20180954 Lian Abril Tume Cusi
+ * Created on 6 de agosto de 2024, 12:06 AM
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+//#include "funciones.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
