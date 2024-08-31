d1ba41bdb429d67a61ce93b2bd4b3e83036214d5
Lian Tume <lian.tume@pucp.edu.pe>
2024-08-31 17:51:24 -0500

    Librerias

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: 20180954 Lian Abril Tume Cusi
- * Created on 6 de agosto de 2024, 12:06 AM
- */
-
 #include <cstdlib>
-#include <fstream>
 #include <iostream>
 #include <iomanip>
-//#include "funciones.h"
+#include <limits>
 
-using namespace std;
 
-int main(int argc, char** argv) {
-
-    return 0;
-}
 
